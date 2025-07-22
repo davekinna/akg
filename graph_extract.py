ae1f248d55b9a18b5a9f922e871a95a189e186bb
Dave Kinna <6689601+davekinna@users.noreply.github.com>
2025-07-22 19:59:14 +0100

    graph_extract more useful

--- graph_extract.py
+++ graph_extract.py
@@ -1,6 +1,8 @@
+import json
+import os
 import argparse
 from rdflib import Graph, Namespace, URIRef, Literal, query
-from akg import AKGException
+from akg import AKGException, FilenameUUIDMap
 # from akg import BIOLINK, ENSEMBL, NCBIGENE, RDFS, RDF, SCHEMA, EDAM, DOI, DCT, PMC, OWL, MONARCH, URN
 
 def load_graph(filename: str) -> Graph:
@@ -19,7 +21,7 @@ def main():
     """
     function for extracting info from one of our graph files
     """
-    parser = argparse.ArgumentParser(description="Load an RDF graph and perform SPARQL queries.")
+    parser = argparse.ArgumentParser(description="Load an RDF graph and extract information.")
     parser.add_argument("filename", type=str, help="Path to the RDF graph file in NT format")
     parser.add_argument("-t", "--triples", action="store_true", help="Count the number of triples in the graph")
     parser.add_argument("-v", "--verbose", action="store_true", help="Print detailed output (all triples in graph)")
@@ -29,12 +31,24 @@ def main():
     parser.add_argument('-g',"--genes", action='store_true', help="List all distinct genes (any category)")
     parser.add_argument("-c", "--count", action="store_true", help="Output counts only for the gene categories")
     parser.add_argument('-p', "--pmid", action='store_true', help="List all distinct PubMed IDs")
+    parser.add_argument('-d', "--datasets", action='store_true', help="List all distinct datasets and row counts")
     args = parser.parse_args()
 
+    if not os.path.exists(args.filename):
+        raise AKGException(f"File {args.filename} does not exist.")
+    else:
+        # check whether filename_uuid_map.json exists in the same directory
+        filename_uuid_map_path = os.path.join(os.path.dirname(args.filename), "filename_uuid_map.json")
+        if not os.path.exists(filename_uuid_map_path):
+            print(f"Warning: {filename_uuid_map_path} does not exist in the same directory. UUIDs to filename check will not be available.")
+        else:
+            filename_uuid_map = FilenameUUIDMap(filename=filename_uuid_map_path)
+    
     graph = load_graph(args.filename)
 
     if args.summary:
-        print(f"Graph loaded from {args.filename} with {len(graph)} triples.")
+        print(f"Graph loaded from: {args.filename}")
+        print(f"Triples: {len(graph)}")
         # calculate the number of distinct subjects, predicates, and objects
         subjects = set()
         predicates = set()
@@ -43,10 +57,31 @@ def main():
             subjects.add(s)
             predicates.add(p)
             objects.add(o)
-        print(f"Distinct subjects: {len(subjects)}, predicates: {len(predicates)}, objects: {len(objects)}")
+        print(f"Subjects: {len(subjects)}")
+        print(f"Predicates: {len(predicates)}")
+        print(f"Objects: {len(objects)}")
+
+    if args.pmid:
+        # Listing all distinct PubMed IDs
+        # note the edam url is not https.
+        pmid_query = """
+        PREFIX EDAM: <http://edamontology.org/>
+        PREFIX PMC: <https://pubmed.ncbi.nlm.nih.gov/>
+        SELECT DISTINCT ?pmid
+        WHERE {
+            ?subject EDAM:has_output ?object
+            FILTER(STRSTARTS(STR(?subject), "https://pubmed.ncbi.nlm.nih.gov/"))
+            BIND(REPLACE(STR(?subject), "https://pubmed.ncbi.nlm.nih.gov/", "") AS ?pmid)
+        }
+    """
+
+        # Execute the SPARQL query to find distinct PubMed IDs
+        results = graph.query(pmid_query)
+        for row in results:
+            print(f"PubMed ID: {row.pmid}")
 
     if args.triples:
-        print(f"Number of triples in the graph: {len(graph)}")
+        print(f"Triples: {len(graph)}")
 
     if args.hgnc:
         gene_query = """
@@ -126,25 +161,75 @@ def main():
 
         print(f"Total distinct genes: {len(resultsHGNC) + len(resultsENS) + len(resultsNCBI) + len(resultsSYM)}")
 
-    if args.pmid:
-        print('Listing all distinct PubMed IDs:')
-        # note the edam url is not https.
-        pmid_query = """
+
+    if args.datasets:
+        # Listing all distinct datasets
+        dataset_query = """
         PREFIX EDAM: <http://edamontology.org/>
-        PREFIX PMC: <https://pubmed.ncbi.nlm.nih.gov/>
-        SELECT DISTINCT ?pmid
+        SELECT DISTINCT ?dataset
         WHERE {
-            ?subject EDAM:has_output ?object
+            ?subject EDAM:has_output ?dataset .
             FILTER(STRSTARTS(STR(?subject), "https://pubmed.ncbi.nlm.nih.gov/"))
-            BIND(REPLACE(STR(?subject), "https://pubmed.ncbi.nlm.nih.gov/", "") AS ?pmid)
         }
-    """
+        GROUP BY ?dataset
+        ORDER BY ?dataset
+        """
 
-        # Execute the SPARQL query to find distinct PubMed IDs
-        results = graph.query(pmid_query)
+        # Execute the SPARQL query to find distinct datasets and their row counts
+        results = graph.query(dataset_query)
+        dataset_uuid = ""
+        dataset_uuid_trim = ""
         for row in results:
-            print(f"PubMed ID: {row.pmid}")
+            # the uuids are prefaced with urn:uuid:,. remove this prefix
+            dataset_uuid = str(row.dataset)
+            dataset_uuid_trim = dataset_uuid.replace("urn:uuid:", "")
+            fromuuid = filename_uuid_map.get_filename_from_uuid(dataset_uuid_trim)
+            if fromuuid is None:
+                print(f"Dataset: {row.dataset}, no corresponding file found in filename_uuid_map.json")
+            else:
+                print(f"Dataset: {row.dataset}, filename from uuid: {fromuuid}")
 
+            # Listing all distinct rows for this dataset
+            # note the edam url is not https. The rows are the objects for which the subject is NOT a PMID
+            row_query = f"""
+            PREFIX EDAM: <http://edamontology.org/>
+            PREFIX PMC: <https://pubmed.ncbi.nlm.nih.gov/>
+            PREFIX URN: <urn:uuid:>
+            SELECT DISTINCT ?data_row
+            WHERE {{
+                URN:{dataset_uuid_trim} EDAM:has_output ?object
+                BIND(?object AS ?data_row)
+            }}
+            """
+
+            # Execute the SPARQL query to find distinct rows
+            row_results = graph.query(row_query)
+            if len(row_results) == 0:
+                print("No distinct rows found.")
+            else:
+                print(f"Distinct rows found: {len(row_results)}")
+                if not args.count:
+                    # we found some, it's worth loading the row labels
+                    try:
+                        rulfile = args.filename + '.row_uri_labels.json'
+                        if os.path.exists(rulfile):
+                            print(f'Loading row_uri_labels from {rulfile}')
+                            with open(rulfile, 'r') as f:
+                                row_uri_labels = json.load(f)
+                                print(f'Loaded {len(row_uri_labels)} row labels from {rulfile}')
+                        else:
+                            print(f'File {rulfile} does not exist in the same directory. Row labels not available.')
+                            row_uri_labels = {}
+                    except FileNotFoundError:
+                        row_uri_labels = {}
+
+                    for row in row_results:
+                        # the uuids are prefaced with urn:uuid:,. remove this prefix
+                        if str(row.data_row) in row_uri_labels:
+                            data_row_uuid = row_uri_labels[str(row.data_row)]
+                        else:
+                            data_row_uuid = "none found"
+                        print(f"Data row: {row.data_row}, label: {data_row_uuid}")
 
     if args.verbose:
         query = """
